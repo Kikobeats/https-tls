bdf841eb7a5e683f4bfe8d423a75646f6be33cd6
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-12-30 09:02:00 +0100

    chore(postinstall): handle res.ok

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,7 +2,15 @@
 
 const { writeFile } = require('fs/promises')
 
-fetch('https://raw.githack.com/apify/fingerprint-suite/master/packages/header-generator/src/data_files/headers-order.json')
-  .then(res => res.json())
-  .then(headersOrder => writeFile('src/headers-order.json', JSON.stringify(headersOrder, null, 2)))
+const URL =
+  'https://raw.githack.com/apify/fingerprint-suite/master/packages/header-generator/src/data_files/headers-order.json'
+
+fetch(URL)
+  .then(res => {
+    if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`)
+    return res.json()
+  })
+  .then(headersOrder =>
+    writeFile('src/headers-order.json', JSON.stringify(headersOrder, null, 2))
+  )
   .catch(error => console.error(error) || process.exit(1))
